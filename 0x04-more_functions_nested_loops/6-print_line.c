2a1b912c1b32843b75198ec312aeca754130ece7
mohamedlakhbab <mohamedlakhbab9@gmail.com>
2023-06-22 17:20:42 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_line - _
+ * @n: nbr of underscores
+ *
+ * Return: void
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0 ; i < n ; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
+
